56b05435d84844b15904ab1d1dc6ec42e060feb6
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-29 16:18:21 +0200

    d

--- restaurant.js
+++ restaurant.js
@@ -1,54 +1,94 @@
 class Restaurant {
-    constructor(mitarbeiter, adresse, tische, speisekarte) {
-        this.mitarbeiter = mitarbeiter;
-        this.adresse = adresse;
-        this.tische = tische;
+    constructor(speisekarte, kasse) {
         this.speisekarte = speisekarte;
+        this.kasse = kasse;
+        this.geoeffnet = false;
+    }
+    personalBezahlen() {
+        //..
+    }
+    oeffnen() {
+        this.geoeffnet = true;
+    }
+    schliessen() {
+        this.geoeffnet = false;
+    }
+    status() {
+        console.log("geoeffnet?", this.geoeffnet);
     }
 }
-const leoRestaurant = new Restaurant(
-    35,
-    "Maximilianstraße 5, 80796 Muenchen",
-    100,
-    "a3"
-);
-console.log(leoRestaurant);
-const speisekarte = {
-    fischgericht: "14€",
-    fleischgericht: "16€",
-    veganesessen: "10€",
-};
-class Personal {
-    constructor(gehalt, arbeitsstunden, urlaub) {
-        this.gehalt = gehalt;
-        this.arbeitsstunden = arbeitsstunden;
-        this.urlaub = urlaub;
-    }
-}
-const leoRestaurantPersonal = new Personal("96€", 8, 30);
-console.log(leoRestaurantPersonal);
+const restaurant = new Restaurant([], 0);
+restaurant.oeffnen();
+restaurant.status();
 
-class Kellner extends Personal {
-    constructor(bestellungenaufnehmen, gehalt, arbeitsstunden, urlaub) {
-        super(gehalt, arbeitsstunden, urlaub);
-        this.bestellungenaufnehmen = bestellungenaufnehmen;
+// const leoRestaurant = new Restaurant();
+// console.log(leoRestaurant);
+// const speisekarte = {
+//     fischgericht: "14€",
+//     fleischgericht: "16€",
+//     veganesessen: "10€",
+//     alkoholischesgetränk: "8€",
+//     unalkhoolischegetränk: "7€",
+// };
+// class Personal {
+//     constructor(gehalt, arbeitsstunden, urlaub) {
+//         this.gehalt = gehalt;
+//         this.arbeitsstunden = arbeitsstunden;
+//         this.urlaub = urlaub;
+//     }
+// }
+// const leoRestaurantPersonal = new Personal("96€", 8, 30);
+// console.log(leoRestaurantPersonal);
+
+// class Kellner extends Personal {
+//     constructor(bestellungenaufnehmen, gehalt, arbeitsstunden, urlaub) {
+//         super(gehalt, arbeitsstunden, urlaub);
+//         this.bestellungenaufnehmen = bestellungenaufnehmen;
+//     }
+// }
+
+// const leoRestaurantKellner = new Kellner("30 täglich", "96€", 8, 30);
+// console.log(leoRestaurantKellner);
+
+// class Koch extends Personal {
+//     constructor(gehalt, arbeitsstunden, urlaub, kochen, bestellungenaufnehmen) {
+//         super(gehalt, arbeitsstunden, urlaub, bestellungenaufnehmen);
+//         this.kochen = kochen;
+//     }
+// }
+// const leoRestaurantKoch = new Koch(
+//     "96€",
+//     8,
+//     30,
+//     "300 Gerichte täglich",
+//     "30mal täglich"
+// );
+// console.log(leoRestaurantKoch);
+
+class Gast {
+    constructor(kellner) {
+        this.kellner = kellner;
+    }
+    bestellen(gericht) {
+        this.gericht = gericht;
+        this.kellner.bestellungenaufnehmen(gericht);
+    }
+    bezahlen() {
+        console.log(`zu bezahlen: ${this.gericht.preis}€`);
+        this.kellner.abkassieren(this.gericht);
     }
 }
+// const leoRestaurantGäste = new Gäste(
+//     `${speisekarte.fischgericht.key} ,${speisekarte.alkoholischesgetränk}`
+// );
+// console.log(leoRestaurantGäste);
+const kellner = new Kellner();
+const gast1 = new Gast(kellner);
+const gast2 = new Gast(kellner);
 
-const leoRestaurantKellner = new Kellner("30 täglich", "96€", 8, 30);
-console.log(leoRestaurantKellner);
+gast1.bestelllen({ name: "Pizza", preis: 10 });
+gast2.bestelllen({ name: "Pasta", preis: 8 });
 
-class Koch extends Personal {
-    constructor(gehalt, arbeitsstunden, urlaub, kochen, bestellungenaufnehmen) {
-        super(gehalt, arbeitsstunden, urlaub, bestellungenaufnehmen);
-        this.kochen = kochen;
-    }
-}
-const leoRestaurantKoch = new Koch(
-    "96€",
-    8,
-    30,
-    "300 Gerichte täglich",
-    "30mal täglich"
-);
-console.log(leoRestaurantKoch);
+gast1.bezahlen();
+
+gast2.bezahlen();
